0657163fb193612885ddfa528e35f282d03554de
united_pooh <united_pooh@icloud.com>
2026-03-22 10:48:50 +0800

    :sparkle: feat: Add methods to modify hit points, magic points, and sanity with boundary checks and tests

--- src/cards/domain/card.py
+++ src/cards/domain/card.py
@@ -45,3 +45,34 @@ class InvestigatorCard:
             player=player,
             cthulhu_mythos=cthulhu_mythos,
         )
+
+    def modify_hit_point(self, amount: int) -> None:
+        self.state.hit_points += amount
+        # 边界约束
+        if self.state.hit_points > self.derived.hit_points_max:
+            self.state.hit_points = self.derived.hit_points_max
+        if self.state.hit_points < 0:
+            self.state.hit_points = 0
+            # TODO: hp <= 0 进入 濒死/死亡 状态变更
+
+        # TODO: amount 数值过大进入额外的判断
+
+
+    def modify_magic_point(self, amount: int):
+        self.state.magic_points += amount
+        if self.state.magic_points > self.derived.magic_points_max:
+            self.state.magic_points = self.derived.magic_points_max
+        if self.state.magic_points < 0:
+            self.state.magic_points = 0
+
+    def modify_sanity(self, amount: int) -> None:
+        """修改当前SAN, 且不能超过 SAN Max"""
+        self.state.sanity += amount
+        if self.state.sanity > self.derived.sanity_max:
+            self.state.sanity = self.derived.sanity_max
+        if self.state.sanity < 0:
+            self.state.sanity = 0
+            # TODO: san <= 0 状态变更
+
+        # TODO: amount 数值过大进入额外的判断
+
